faf5f9c80ebc7bf07d0b946201d527432f82a271
athena272 <guilhermera272@gmail.com>
2023-10-03 04:58:09 -0300

    add project

---
+++ src/database/index.js
@@ -0,0 +1,20 @@
+const { Client } = require('pg')
+
+const client = new Client({
+  host: 'localhost',
+  port: 5432,
+  user: 'root',
+  password: 'root',
+  database: 'hotel_management',
+})
+
+client.connect()
+  .then(() => console.log('🔥 Connected to database 🔥'))
+  .catch((err) => console.log('💀 Error connecting to database', err))
+
+exports.query = async (query, values) => {
+  const { rows } = await client.query(query, values)
+  return rows
+}
+
+// client.query('SELECT * FROM contacts').then(result => console.log(result))
