6b63d9ed287287a0cf7315deab7eb31da9909ff2
athena272 <guilhermera272@gmail.com>
2023-10-03 06:52:46 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -5,10 +5,17 @@ const ContactController = require('./app/controllers/ContactController')
 // const route = express.Router()
 const router = Router()
 
+// router.get('/contacts', ContactController.index)
+// router.get('/contacts/:id', ContactController.show)
+// router.delete('/contacts/:id', ContactController.delete)
+// router.post('/contacts', ContactController.store)
+// router.put('/contacts/:id', ContactController.update)
+//Categorias
 router.get('/contacts', ContactController.index)
 router.get('/contacts/:id', ContactController.show)
 router.delete('/contacts/:id', ContactController.delete)
 router.post('/contacts', ContactController.store)
 router.put('/contacts/:id', ContactController.update)
+
 
 module.exports = router
