113c0bd3058ef9d8828d50b2f2d727936ee420cb
athena272 <guilhermera272@gmail.com>
2023-10-03 08:23:15 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,21 +1,24 @@
 const { Router } = require('express')
-const ContactController = require('./app/controllers/ContactController')
+const CategoriasController = require('./app/controllers/CategoriasController')
 // const { index } = require('./app/controllers/ContactController')
 
 // const route = express.Router()
 const router = Router()
 
-// router.get('/contacts', ContactController.index)
-// router.get('/contacts/:id', ContactController.show)
-// router.delete('/contacts/:id', ContactController.delete)
-// router.post('/contacts', ContactController.store)
-// router.put('/contacts/:id', ContactController.update)
+
 //Categorias
-router.get('/contacts', ContactController.index)
-router.get('/contacts/:id', ContactController.show)
-router.delete('/contacts/:id', ContactController.delete)
-router.post('/contacts', ContactController.store)
-router.put('/contacts/:id', ContactController.update)
+router.get('/categorias', CategoriasController.index)
+router.get('/categorias/:id', CategoriasController.show)
+router.delete('/categorias/:id', CategoriasController.delete)
+router.post('/categorias', CategoriasController.store)
+router.put('/categorias/:id', CategoriasController.update)
+
+// //Usuarios
+// router.get('/usuarios', ContactController.index)
+// router.get('/usuarios/:id', ContactController.show)
+// router.delete('/usuarios/:id', ContactController.delete)
+// router.post('/usuarios', ContactController.store)
+// router.put('/usuarios/:id', ContactController.update)
 
 
 module.exports = router
