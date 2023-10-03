074f6c0a29d230e3d7ae2b652f841d8c2d35dc7f
athena272 <guilhermera272@gmail.com>
2023-10-03 11:35:55 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 const { Router } = require('express')
 const CategoriasController = require('./app/controllers/CategoriasController')
+const UsersController = require('./app/controllers/UsersController')
 // const { index } = require('./app/controllers/ContactController')
 
 // const route = express.Router()
@@ -13,12 +14,12 @@ router.delete('/categorias/:id', CategoriasController.delete)
 router.post('/categorias', CategoriasController.store)
 router.put('/categorias/:id', CategoriasController.update)
 
-// //Usuarios
-// router.get('/usuarios', ContactController.index)
-// router.get('/usuarios/:id', ContactController.show)
-// router.delete('/usuarios/:id', ContactController.delete)
-// router.post('/usuarios', ContactController.store)
-// router.put('/usuarios/:id', ContactController.update)
+//Usuarios
+router.get('/usuarios', UsersController.index)
+router.get('/usuarios/:id', UsersController.show)
+router.delete('/usuarios/:id', UsersController.delete)
+router.post('/usuarios', UsersController.store)
+router.put('/usuarios/:id', UsersController.update)
 
 
 module.exports = router
